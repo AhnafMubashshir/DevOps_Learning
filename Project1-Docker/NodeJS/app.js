5eabeceffeb2419e3d8ee0992cd479a765539e94
galib-1206 <bsse1206@iit.du.ac.bd>
2024-01-24 15:25:25 +0600

    header eror fixed & run hello world

--- Project1-Docker/NodeJS/app.js
+++ Project1-Docker/NodeJS/app.js
@@ -3,6 +3,9 @@ const app= express()
 
 app.get('/',(req,res)=> res.send('hello World'))
 
-// app.listen(3000,()=>console.log('server ready'))
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+
+app.listen(port, () => 
+{console.log(`Server listening on port ${port}`)
+console.log(`Hello World`)}
+);
