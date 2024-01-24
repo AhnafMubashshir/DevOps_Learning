311ba386b02077306512be73114912eca88daa59
galib-1206 <bsse1206@iit.du.ac.bd>
2024-01-24 18:57:55 +0600

    Print in json format

--- Project1-Docker/NodeJS/app.js
+++ Project1-Docker/NodeJS/app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app= express()
 
-app.get('/',(req,res)=> res.send('hello World'))
+// app.get('/',(req,res)=> res.send('hello World'))
+app.get('/',(req,res)=> res.json({message:'hello World'}))
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => 
-{console.log(`Server listening on port ${port}`)
-console.log(`Hello World`)}
-);
+app.listen(port, () => {
+console.log(`Server listening on port ${port}`)
+});
